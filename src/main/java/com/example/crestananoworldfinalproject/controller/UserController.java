6ffcdd00e1a6dcbcc2c25e6a92f0dd80ad4015c9
JosefinaCresta <josefinacresta@gmail.com>
2022-09-14 21:11:04 +0200

    update

---
+++ src/main/java/com/example/crestananoworldfinalproject/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.example.crestananoworldfinalproject.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+    @Autowired
+    private UserServiceInterface userServiceInterface;
+
+    @GetMapping("/users")
+    @ResponseStatus(HttpStatus.OK)
+    public List<User> getUsers() {
+        return userServiceInterface.getUsers();
+    }
+}
+
---
+++ src/main/java/com/example/crestananoworldfinalproject/controller/UserServiceInterface.java
@@ -0,0 +1,15 @@
+package com.example.crestananoworldfinalproject.controller;
+
+
+import java.util.List;
+
+public interface UserServiceInterface {
+
+
+    List<User> getUsers();
+
+}
+
+
+
+
