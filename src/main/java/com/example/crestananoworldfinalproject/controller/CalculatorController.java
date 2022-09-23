566c1d3aba69fa6bad8ea1d6fcbfce85a75cb08b
JosefinaCresta <josefinacresta@gmail.com>
2022-09-23 15:27:26 +0200

    crossOrigin CLuster

--- src/main/java/com/example/crestananoworldfinalproject/controller/CalculatorController.java
+++ src/main/java/com/example/crestananoworldfinalproject/controller/CalculatorController.java
@@ -13,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin
 public class CalculatorController {
     @Autowired
     private CalculatorRepository calculatorRepository;
